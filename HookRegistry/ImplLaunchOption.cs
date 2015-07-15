b5015cddeef83888f6b630ca6c73e63f48b274c8
laiqu <laiquendir@gmail.com>
2015-07-15 21:47:27 +0200

    Implement GetLaunchOption

---
+++ HookRegistry/ImplLaunchOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hooks
+{
+	[RuntimeHookAttribute]
+	public class ImplLaunchOption
+	{
+		public ImplLaunchOption()
+		{
+			HookRegistry.Register (OnCall);
+		}
+
+		object OnCall(string typeName, string methodName, object this_obj, params object[] args) {
+			if (typeName != "BattleNet" || methodName != "GetLaunchOption") {
+				return null;
+			}
+			return "enUS";
+		}
+	}
+}
+
